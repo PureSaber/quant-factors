2f748f9dc2fe942b461ce766865a68ddf635449c
Neko65536 <kangfx918@163.com>
2026-08-27 18:16:10 +0800

    feat: add time-series validation primitives

--- src/quant_factors/__init__.py
+++ src/quant_factors/__init__.py
@@ -7,6 +7,23 @@ from quant_factors.core import compute_factors, list_factors
 try:
     __version__ = version("quant-factors")
 except PackageNotFoundError:  # pragma: no cover - editable/source tree
-    __version__ = "0.1.0"
+    __version__ = "0.2.0"
 
-__all__ = ["__version__", "compute_factors", "list_factors"]
+from quant_factors.validation import (
+    ValidationSplit,
+    audit_feature_availability,
+    benjamini_hochberg,
+    purged_kfold_splits,
+    walk_forward_splits,
+)
+
+__all__ = [
+    "__version__",
+    "ValidationSplit",
+    "audit_feature_availability",
+    "benjamini_hochberg",
+    "compute_factors",
+    "list_factors",
+    "purged_kfold_splits",
+    "walk_forward_splits",
+]
